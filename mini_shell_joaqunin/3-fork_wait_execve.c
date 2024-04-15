722053f9dc693248a4dab73476f396511606082f
AguilarJ11 <8664@holbertonstudents.com>
2024-04-15 15:02:17 +0000

    fork-wait-execve

---
+++ mini_shell_joaqunin/3-fork_wait_execve.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/type.h>
+#include <sys/wait.h>
+
+/**
+ *
+ *
+ */
+
+int main (void)
+{
+	char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
+	int cont, status;
+	pid_t = pid;
+
+	for (cont = 0; cont < 5; cont++)
+	{
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Error:");
+		return (1);
+	}
+
+	if (pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+			perror("Error:");
+		return (0);
+	}
+	else
+	{
+		wait(&status);
+	}
+
+	return (0);
+	}
+
+
+}
