2cd3a1c95f4567df0d34b8ea9c751198d053e8eb
AguilarJ11 <8664@holbertonstudents.com>
2024-04-17 00:24:39 +0000

    header

---
+++ holyshell.c
@@ -0,0 +1,17 @@
+/*
+* File: holyshell.c
+* Auth: Gabriel Morffe, Joaquin Aguilar
+*/
+
+#include "zzz.h"
+
+/**
+ * main - simple_shell
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+		return (0);
+}
---
+++ zzz.h
@@ -0,0 +1,14 @@
+#ifndef SHLL
+#define SHLL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(void);
+
+#endif
